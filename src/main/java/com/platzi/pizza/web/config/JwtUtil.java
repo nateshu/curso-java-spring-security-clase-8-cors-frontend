1837f2311860d4332cdd05525fd2e31867e65af4
Alvaro Nates <nateshu@hotmail.com>
2023-12-16 10:12:00 -0500

    Jason token

--- src/main/java/com/platzi/pizza/web/config/JwtUtil.java
+++ src/main/java/com/platzi/pizza/web/config/JwtUtil.java
@@ -2,6 +2,7 @@ package com.platzi.pizza.web.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -20,4 +21,24 @@ public class JwtUtil {
                 .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)))
                 .sign(ALGORITHM);
     }
+
+
+    public boolean isValid(String jwt) {
+        try {
+            JWT.require(ALGORITHM)
+                    .build()
+                    .verify(jwt);
+            return true;
+        } catch (JWTVerificationException e) {
+            return false;
+        }
+    }
+
+    public String getUsername(String jwt) {
+        return JWT.require(ALGORITHM)
+                .build()
+                .verify(jwt)
+                .getSubject();
+    }
+
 }
